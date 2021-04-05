95d4e0eb951c0701694f148977bc1d1eb67323de
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2021-04-05 14:34:05 +0200

    Experiment with ejs layouts

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-const path = require('path');
-const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
 const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 
 const config = require('./config');
@@ -15,19 +16,24 @@ const dbURI ='mongodb://mongo:27017/PersonalWebsite';
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then((result) => { app.listen(PORT, () => { console.log("Server is listening at port " + PORT) }) })
     .catch((err) => console.log(err));
+// app.listen(PORT, () => { console.log("Server is listening at port " + PORT) });
 
 // ------------- MIDDLEWARES -------------
 app.use(express.json()); //receive json from HTTP POST
 app.use(fileUpload()); //middleware for easy uploading of files
 app.use(cors());
 
+app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.set('public', path.join(__dirname, 'public'));
+app.use(express.static('public'));
 
 // ------------- ROUTES -------------
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.use(express.static('public'));
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
