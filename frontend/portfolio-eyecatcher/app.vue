a0e49a72de0e0321c4e8761545f01ece3c3eb62c
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2022-05-19 17:03:12 +0200

    Add Navbar

--- frontend/portfolio-eyecatcher/app.vue
+++ frontend/portfolio-eyecatcher/app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <header class="w-full bg-gray-100 sticky top-0">
+    <div class="max-w-6xl m-auto px-6">
+      <Navbar />
+    </div>
+  </header>
+  <div class="m-auto max-w-6xl px-6">
+    <NuxtPage />
   </div>
 </template>
---
+++ frontend/portfolio-eyecatcher/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Welcome home!</h1>
+  <NuxtLayout name="project-page">
+  test
+  </NuxtLayout>
+</template>
+
+<script>
+definePageMeta({
+  layout: false,
+});
+</script>
