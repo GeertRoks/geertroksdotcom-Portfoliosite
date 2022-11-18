0dbf35b1b918d5086792f21303d39489558db13d
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2022-11-18 14:22:56 +0100

    Add appache-style index for static file server

--- static-file-server/server.js
+++ static-file-server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const serveIndex = require('serve-index');
 const app = express();
 const port = 3434;
 
 app.use(express.static('public'));
+app.use('/images', express.static('public/images'), serveIndex('public/images', {'icons': true}));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
